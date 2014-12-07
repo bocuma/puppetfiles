57ee8b5c417ef941ebc1bbb8d38a2b91ee4c1170
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-07 16:30:41 +0000

    Revert "if defined gemset"
    This reverts commit 5be06da4385be979960efa9ee753fb8a47c50f9b.

--- environments/production/manifests/god.pp
+++ environments/production/manifests/god.pp
@@ -2,12 +2,10 @@ define bocuma::god ($state = 'present', $ruby = 'ruby-2.0.0-p451', $version = 'l
   if !defined(Class['rvm']) {
    include rvm
   }
-  if !defined(Rvm_gemset["$ruby@$gemset"]) {
-    rvm_gemset {
-      "$ruby@$gemset":
-        ensure  => $state,
-        require => Rvm_system_ruby[$ruby];
-    }
+  rvm_gemset {
+    "$ruby@$gemset":
+      ensure  => $state,
+      require => Rvm_system_ruby[$ruby];
   }
   rvm_gem {
     "$ruby@$gemset/god":
