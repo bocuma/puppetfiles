5be06da4385be979960efa9ee753fb8a47c50f9b
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-07 15:02:22 +0000

    if defined gemset

--- environments/production/manifests/god.pp
+++ environments/production/manifests/god.pp
@@ -2,10 +2,12 @@ define bocuma::god ($state = 'present', $ruby = 'ruby-2.0.0-p451', $version = 'l
   if !defined(Class['rvm']) {
    include rvm
   }
-  rvm_gemset {
-    "$ruby@$gemset":
-      ensure  => $state,
-      require => Rvm_system_ruby[$ruby];
+  if !defined(Rvm_gemset["$ruby@$gemset"]) {
+    rvm_gemset {
+      "$ruby@$gemset":
+        ensure  => $state,
+        require => Rvm_system_ruby[$ruby];
+    }
   }
   rvm_gem {
     "$ruby@$gemset/god":
