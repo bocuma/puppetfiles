a99d0cf562d028a9dbdc5fc73fbcdaacee7b93a6
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-07 19:27:20 +0000

    init script god
    Conflicts:
    	modules/god/init-god.sh.erb

--- environments/production/manifests/god.pp
+++ environments/production/manifests/god.pp
@@ -13,12 +13,20 @@ define bocuma::god ($state = 'present', $ruby = 'ruby-2.0.0-p451', $version = 'l
       ensure => "directory"
   }
 
+  $ruby_path = "/usr/local/rvm"
+  $ruby_version = "$ruby@$gemset"
 
   file {"god-config": 
     path => "/etc/god/god.conf",
     require => File["god-dir"],
     content => "God.load \"/etc/god/conf.d/*.god\""
   }
+
+  file {"god-init-script": 
+    path => "/etc/init.d/god",
+    content => template("god/god.init")
+  }
+
   if (!defined(Rvm_system_ruby[$ruby])) {
     rvm_system_ruby {
       $ruby:
@@ -29,21 +37,21 @@ define bocuma::god ($state = 'present', $ruby = 'ruby-2.0.0-p451', $version = 'l
   }
 
   rvm_gemset {
-    "$ruby@$gemset":
+    "$ruby_version":
       ensure  => $state,
       require => Rvm_system_ruby[$ruby];
   }
   rvm_gem {
-    "$ruby@$gemset/god":
+    "$ruby_version/god":
       ensure  => $version,
-      require => Rvm_gemset["$ruby@$gemset"];
+      require => Rvm_gemset["$ruby_version"];
   }
   rvm_wrapper {
     'god':
       target_ruby => $ruby,
       prefix      => 'bootup',
       ensure      => $state,
-      require => Rvm_gemset["$ruby@$gemset"];
+      require => Rvm_gemset["$ruby_version"];
   }
 }
 
