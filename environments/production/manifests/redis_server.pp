a7f09cfd8186b78db40066ed545adc3fce498bd5
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-13 20:14:03 +0000

    redis god

--- environments/production/manifests/redis_server.pp
+++ environments/production/manifests/redis_server.pp
@@ -1,3 +1,12 @@
 class bocuma::redis_server {
+  $port = 6379
   include redis
+  god::process { "redis_$port":
+    name => "redis_$port",
+    start_command => "/etc/init.d/redis_$port start",
+    stop_command => "/etc/init.d/redis_$port stop",
+    restart_command => "/etc/init.d/redis_$port restart",
+    pidfile => "/var/run/redis_$port.pid"
+  }
+
 }
