8c030c1ec3940928508722c7fdb20c8db6be5055
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-13 11:41:36 +0000

    updated god & used for mongod

--- environments/production/manifests/mongodb.pp
+++ environments/production/manifests/mongodb.pp
@@ -1,20 +1,26 @@
-class bocuma::mongodb {
+class bocuma::mongodb ($replset = "prod0", $pidfilepath = "/var/run/mongod.pid", $logpath => "/var/log/mongod/mongod.pid", $dbpath = "/srv/databases/mongodb", $config = "/etc/mongod.conf") {
   class {'::mongodb::globals':
-  manage_package_repo => true,
+    manage_package_repo => true,
   }->
   class { '::mongodb::server': 
-    bind_ip => ["0.0.0.0"]
+    bind_ip => ["0.0.0.0"],
+    replset => $replset,
+    pidfilepath => $pidfilepath,
+    dbpath => $dbpath,
+    config => $config,
+    logpath => $logpath
   } ->
+  bocuma::logrotate { "mongod":
+    name => "mongod",
+    path => $logpath
+  }
+  god::process { "mongod":
+    name => "mongod",
+    start_command => "/etc/init.d/mongod start",
+    stop_command => "/etc/init.d/mongod stop",
+    restart_command => "/etc/init.d/mongod restart",
+    pidfile => $pidfilepath
+  }
   class {'::mongodb::client':}
 
-  #package {"monit":
-  #  ensure => present
-  #}
-  #service {"monit":
-  #  ensure => running
-  #}
-
-  #monit::monitor {"mongod":
-  #  pidfile => '/var/run/mongodb/mongod.pid'
-  #}
 }
